2df2cdeb0b7ef55caaae303f486df78d43ef6d8e
Barbora Hornackova <bara@storytlrs.cz>
2018-04-29 15:27:12 +0200

    Initial app

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../header/Header';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
