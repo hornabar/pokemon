2f51ac73e6b1e854b17f2eab956a3c7712e0d53d
Barbora Hornackova <bara@storytlrs.cz>
2018-04-29 15:28:30 +0200

    Add header

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => (
+	<header class="header">
+		<div class="header__container">
+			<div class="header__logo">
+				Pokemons App
+			</div>
+		</div>
+	</header>
+);
+
+export default Header;
